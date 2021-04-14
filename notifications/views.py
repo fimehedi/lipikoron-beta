e94f007327e7f1119d7f6176d74a737b2fa98db7
Fokrul Islam Mehedi <fokrulislammehedi2000@gmail.com>
2021-04-14 15:51:27 +0600

    notification count view added

--- notifications/views.py
+++ notifications/views.py
@@ -8,7 +8,15 @@ from .models import Notification
 def getNotification(request):
     user = request.user
     notifications = Notification.objects.filter(user=user).order_by('-date')
+    old_notifications = notifications.filter(is_seen=True)
+    new_notifications = notifications.filter(is_seen=False)
+    print(new_notifications)
 
-    return render(request, 'notification.html', context={
-        'notifications': notifications
+    response = render(request, 'notification.html', context={
+        'old_notifications': old_notifications,
+        'new_notifications': new_notifications
     })
+    new_notifications.update(is_seen=True)
+    print(new_notifications)
+
+    return response
